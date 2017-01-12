d3b8baf08ab24291e2fc5bf773ab33880026a217
Jinzhu Li <jinzhu@cs.ualberta.ca>
2017-01-11 18:03:38 -0700

    update

--- checkversion.py
+++ checkversion.py
@@ -5,7 +5,8 @@ import requests
 print requests.__version__
 
 #sth like curl
-response = requests.get("http://google.com/") #get script for google
+response = requests.get("https://raw.githubusercontent.com/Aries0331/c404Lab/master/checkversion.py") 
+#get script for google "http://google.com"
 #request is automatically following so instead we get google.ca
 
 print response.text
